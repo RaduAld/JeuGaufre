e2f2f267425fca2c965b902ab19a05c6bad844ad
dariyoka <172923949+dariyoka@users.noreply.github.com>
2026-04-27 14:25:54 +0200

    Ameliorer interface graphique

--- src/Vue/InterfaceGraphique.java
+++ src/Vue/InterfaceGraphique.java
@@ -31,6 +31,7 @@ public class InterfaceGraphique implements Runnable{
         droite.setLayout(new BoxLayout(droite,BoxLayout.Y_AXIS));
         droite.setBorder(new EmptyBorder(50,50,50,50));
         JPanel gameContainer = new JPanel(new BorderLayout());
+        gameContainer.setBorder(new EmptyBorder(50,50,50,50));
         gauf = new Gaufre(jeu);
         gameContainer.add(gauf, BorderLayout.CENTER);
         joueur = createJLabel("joueur en cours: 1");
@@ -39,17 +40,21 @@ public class InterfaceGraphique implements Runnable{
         sauvegarder= createJButton("sauvegarder");
         restaurer=createJButton("restaurer");
         nouvellePartie=createJButton("nouvelle partie");
-        //droite.add(Box.createGlue());
+        droite.add(Box.createGlue());
         droite.add(joueur,BorderLayout.CENTER);
-        //droite.add(Box.createGlue());
+        droite.add(Box.createVerticalStrut(40));
         droite.add(annuler);
+        droite.add(Box.createVerticalStrut(10));
         droite.add(rejouer);
-        //droite.add(Box.createGlue());
+        droite.add(Box.createGlue());
         droite.add(sauvegarder);
+        droite.add(Box.createVerticalStrut(10));
         droite.add(restaurer);
-        //droite.add(Box.createGlue());
+        droite.add(Box.createGlue());
         droite.add(nouvellePartie);
-        //droite.add(Box.createGlue());
+        droite.add(Box.createGlue());
+        droite.setBackground(new Color(180, 125, 107));
+        gameContainer.setBackground(new Color(41, 16, 7));
         frame.add(gameContainer);
         frame.add(droite,BorderLayout.EAST);
         frame.setVisible(true);
@@ -60,6 +65,9 @@ public class InterfaceGraphique implements Runnable{
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
         button.setFocusable(false);
         button.setMaximumSize(new Dimension(Integer.MAX_VALUE, button.getPreferredSize().height));
+        button.setFont(new Font("Monserrat",Font.BOLD,16));
+        button.setBackground(new Color(224, 209, 197));
+        button.setBorderPainted(false);
         return button;
     }
     private JToggleButton createToggleButton(String texte){
@@ -67,11 +75,16 @@ public class InterfaceGraphique implements Runnable{
         toggleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         toggleButton.setFocusable(false);
         toggleButton.setMaximumSize(new Dimension(Integer.MAX_VALUE, toggleButton.getPreferredSize().height));
+        toggleButton.setFont(new Font("Monserrat",Font.BOLD,16));
+        toggleButton.setBackground(new Color(224, 209, 197));
+        toggleButton.setBorderPainted(false);
         return toggleButton;
     }
     private JLabel createJLabel(String texte){
         JLabel label = new JLabel(texte);
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        label.setFont(new Font("Monserrat",Font.BOLD,18));
+        label.setForeground(new Color(41, 16, 7));
         return label;
     }
     private Box createHorizontalBox(){
