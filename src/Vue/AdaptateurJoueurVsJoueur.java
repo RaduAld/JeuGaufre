1ef769499ecc5399ca330193fb2f1d38145c673b
MERABET Lynda <lyndamerabet1@gmail.com>
2026-04-28 14:48:10 +0200

    ajout btns ia, game over

---
+++ src/Vue/AdaptateurJoueurVsJoueur.java
@@ -0,0 +1,17 @@
+package Vue;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdaptateurJoueurVsJoueur implements ActionListener {
+    CollecteurEvenements control;
+
+    AdaptateurJoueurVsJoueur(CollecteurEvenements c) {
+        control = c;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        control.toucheClavier("JoueurVsJoueur");
+    }
+}
